54287a9844f41a0ed3c88981541e22c9870a2424
Sampson <297511521@qq.com>
2021-02-17 20:02:39 +0800

    Delete miner directory

--- miner/wn-env.sh
+++
@@ -1,4 +0,0 @@
-export PATH=/initial/bin:$PATH
-export FULLNODE_API_INFO=
-export FIL_PROOFS_PARAMETER_CACHE=/initial/params LOTUS_MINER_PATH=/initial/wn-miner
-export IPFS_GATEWAY=https://proof-parameters.s3.cn-south-1.jdcloud-oss.com/ipfs/
